05af553f20e41405395dc58582c3ac0be9801104
andreippsq <andreypopescu99@yahoo.com>
2022-06-15 11:48:27 +0300

    first

---
+++ iosProject/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosProject
+//
+//  Created by Mobile Academy on 15/6/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
